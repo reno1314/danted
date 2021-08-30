c7022d7adb5ccbb5e54f63463a5dc19160a522ad
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-08-31 00:36:37 +0800

    Update install_R.sh

--- install_R.sh
+++ install_R.sh
@@ -13,7 +13,7 @@ if [ $(id -u) != "0" ]; then
 fi
 
 REQUEST_SERVER="https://raw.github.com/reno1314/danted/master"
-SCRIPT_SERVER="https://github.com/reno1314"
+SCRIPT_SERVER="https://github.com/reno1314/sockd.info.git"
 SYSTEM_RECOGNIZE=""
 
 [ "$1" == "--no-github" ] && REQUEST_SERVER=${SCRIPT_SERVER}
