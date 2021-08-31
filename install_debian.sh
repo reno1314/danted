01fb51af13ec0eee8636db8fc86ef1d67ae7a2ea
reno1314 <80469553+reno1314@users.noreply.github.com>
2021-08-31 10:35:40 +0800

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -10,7 +10,7 @@ DEFAULT_USER=""
 DEFAULT_PAWD=""
 WHITE_LIST_NET=""
 WHITE_LIST=""
-SCRIPT_HOST="http://public.reno324.ga"
+SCRIPT_HOST="http://52.229.203.175"
 PACKAGE_NAME="dante_1.3.2-1_$(uname -m).deb"
 COLOR_PATH="/etc/default/color"
 
