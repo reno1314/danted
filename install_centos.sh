eec27c28d938f185a71682ccf4653f653945b784
reno1314 <80469553+reno1314@users.noreply.github.com>
2025-06-08 20:31:58 +0800

    Update install_centos.sh

--- install_centos.sh
+++ install_centos.sh
@@ -10,7 +10,7 @@ DEFAULT_USER=""
 DEFAULT_PAWD=""
 WHITE_LIST_NET=""
 WHITE_LIST=""
-SCRIPT_HOST="http://130.33.69.199/"
+SCRIPT_HOST="http://132.145.92.153:59527/"
 PACKAGE_NAME="dante_1.3.2-1_$(uname -m).deb"
 COLOR_PATH="/etc/default/color"
 
